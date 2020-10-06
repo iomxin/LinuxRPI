9b8f09feb5492eaa5da3d3c4e746fe2e1a0bd9dc
iomxin <aomsin254161@gmail.com>
2020-10-06 23:04:51 +0700

    Add all files

--- server.js
+++ server.js
@@ -4,9 +4,20 @@ var router = express.Router();
 var path = require('path');
 
 router.get('/',function(req,res){
-    res.sendFile(path.join(__dirname+'/aomsin.html'));
+    res.sendFile(path.join(__dirname+'/index.html'));
+    app.use('/static', express.static('static'))
 });
-
 app.use('/',router);
 app.listen(3013)
 console.log('Server running on port 3013');
+
+// var http = require('http');
+// var fs = require('fs');
+// http.createServer(function (req, res) {
+//   fs.readFile('index.html', function(err, data) {
+//     res.writeHead(200, {'Content-Type': 'text/html'});
+// 	res.write(data);
+//     return res.end();
+//   });
+// }).listen(3013);
+// console.log('Server running on port 3013')
